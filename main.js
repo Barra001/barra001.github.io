ae3fa39cf05a7ce49f13a19637b9480a0675aa7b
Barra001 <manubarrabino@gmail.com>
2022-08-18 11:51:09 -0300

    musica supenso

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
-document.addEventListener("keypress", async function(event) {
-   
-    
+var isPlaying = false
+
+document.addEventListener("keypress", async function (event) {
+
+
     if (event.keyCode == 32) {
-   
+
         document.getElementById("cross").classList.add("mostrarCross");
         var audio = new Audio('error-sound.mp3');
         audio.play();
         await sleep(2000)
         document.getElementById("cross").classList.remove("mostrarCross");
     }
-  });
+});
 
 
 function sleep(ms) {
@@ -22,32 +24,32 @@ var preguntas = [
     { pregunta: "¿Que colores hay?", opt1: ["Rojo", 80], opt2: ["Verde", 5], opt3: ["Azul", 5], opt4: ["Amarillo", 5], opt5: ["Naranja", 2], opt6: ["Blanco", 1], opt7: ["Negro", 1], opt8: ["Gris", 1] }
 
 ];
+var contador = document.getElementById("contador")
 
-
-
+selectAvaliableQuestions()
 
 function mostrarRespuesta(opt) {
-    var contador = document.getElementById("contador")
+
     let preguntaActual = parseInt(document.getElementById("actualPregunta").innerText) - 1;
     let opcionActual = document.getElementById(opt);
 
     if (preguntas[preguntaActual][opt][0] != "-" && !opcionActual.classList.contains("correcto")) {
-        opcionActual.innerHTML =  preguntas[preguntaActual][opt][0] +'<div class="number">'+preguntas[preguntaActual][opt][1]+'</div>'
+        opcionActual.innerHTML = preguntas[preguntaActual][opt][0] + '<div class="number">' + preguntas[preguntaActual][opt][1] + '</div>'
         contador.innerText = parseInt(contador.innerText) + preguntas[preguntaActual][opt][1]
-        if(contador.innerText=="100"){
+        if (contador.innerText == "100") {
             animacionContenedor("grande")
             var audioWinner = new Audio('winersSound.mp3');
             audioWinner.play();
         }
-        
+
         opcionActual.classList.add("correcto")
         var audio = new Audio('success-sound.mp3');
         audio.play();
-        
 
 
-    }else{
-        if(contador.innerText=="100"){
+
+    } else {
+        if (contador.innerText == "100") {
             animacionContenedor("grandeMal")
             var audio = new Audio('error.mp3');
             audio.play();
@@ -55,37 +57,83 @@ function mostrarRespuesta(opt) {
     }
 }
 
-async function animacionContenedor(option){
-  
+async function animacionContenedor(option) {
+
     document.getElementById("containerG").classList.add(option);
-    
+
     await sleep(650)
-   
+
     document.getElementById("containerG").classList.remove(option);
 }
 
-async function animacionNumero(){
+async function animacionNumero() {
     document.getElementById("actualPregunta").classList.add("nextNum");
     await sleep(750)
     document.getElementById("actualPregunta").classList.remove("nextNum");
 }
 
-function cargarPregunta() {
+function cargarPregunta(sum) {
+    let contenedorPreguntaNumero = document.getElementById("actualPregunta");
+    let siguientePag = parseInt(contenedorPreguntaNumero.innerText) + sum;
+    if( siguientePag == 0 || siguientePag == preguntas.length +1){
+        var audio = new Audio('error.mp3');
+        audio.play();
+        return
+    }
     var audio = new Audio('next-sound.mp3');
     audio.play();
     animacionNumero()
     contador.innerText = "0"
-    let contenedorPreguntaNumero = document.getElementById("actualPregunta");
-    contenedorPreguntaNumero.innerText = parseInt(contenedorPreguntaNumero.innerText) + 1;
+    
+    contenedorPreguntaNumero.innerText = siguientePag;
     let cont = 1;
-    while(cont !=9){
-        let opcionActual = document.getElementById("opt"+cont);
-        opcionActual.innerHTML=cont
-        if(opcionActual.classList.contains("correcto")){
+    while (cont != 9) {
+        let opcionActual = document.getElementById("opt" + cont);
+        opcionActual.innerHTML = cont
+        if (opcionActual.classList.contains("correcto")) {
             opcionActual.classList.remove("correcto")
         }
-        cont = cont +1
+        if (opcionActual.classList.contains("disabled")) {
+            opcionActual.classList.remove("disabled")
+        }
+        cont = cont + 1
     }
-   
+    selectAvaliableQuestions()
+
+
+}
+
+function selectAvaliableQuestions() {
+    
+    let preguntaActual = parseInt(document.getElementById("actualPregunta").innerText) - 1;
+    let cont = 1;
+    while (cont != 9) {
+        if (preguntas[preguntaActual]["opt"+cont] == "-") {
+            let opcionActual = document.getElementById("opt" + cont);
+            opcionActual.innerHTML = "";
+            opcionActual.classList.add("disabled")
+        }
+        cont = cont + 1
+    }
+}
+var audioMusic = new Audio('suspenso.mp3');
+audioMusic.loop = true
+audioMusic.volume = 0.23
+function togglePlay(){
+    let textElement = document.getElementById("playText")
+    if(isPlaying){
+        
+        textElement.innerText="play_arrow"
+        isPlaying=false
+        audioMusic.pause();
+
+    }else{
+        textElement.innerText="pause"
+        isPlaying=true
+        
+        audioMusic.loop = true
+        audioMusic.play();
+
+    }
     
 }

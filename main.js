94fb37b3932b6cb6948b6460f1e5a63276aa65b8
Barra001 <manubarrabino@gmail.com>
2022-08-23 20:03:37 -0300

    cancelacion y control flechas

--- main.js
+++ main.js
@@ -10,10 +10,9 @@ successAudio.load()
 audioWinner.load()
 errorAudio.load()
 
-document.addEventListener("keypress", async function (event) {
+document.addEventListener("keydown", async function (event) {
 
-
-    if (event.keyCode == 32) {
+    if (event.code == "Space") {
 
         document.getElementById("cross").classList.add("mostrarCross");
         errorAudio.currentTime = 0
@@ -21,45 +20,46 @@ document.addEventListener("keypress", async function (event) {
         await sleep(2000)
         document.getElementById("cross").classList.remove("mostrarCross");
     }
-    if (event.keyCode == 49) {
+    if (event.code == "Numpad1" || event.code == "Digit1") {
 
         mostrarRespuesta('opt1')
     }
-    if (event.keyCode == 50) {
+    if (event.code == "Numpad2" || event.code == "Digit2") {
 
         mostrarRespuesta('opt2')
     }
-    if (event.keyCode == 51) {
+    if (event.code == "Numpad3" || event.code == "Digit3") {
 
         mostrarRespuesta('opt3')
     }
-    if (event.keyCode == 52) {
+    if (event.code == "Numpad4" || event.code == "Digit4") {
 
         mostrarRespuesta('opt4')
     }
-    if (event.keyCode == 53) {
+    if (event.code == "Numpad5" || event.code == "Digit5") {
 
         mostrarRespuesta('opt5')
     }
-    if (event.keyCode == 54) {
+    if (event.code == "Numpad6" || event.code == "Digit6") {
 
         mostrarRespuesta('opt6')
     }
-    if (event.keyCode == 55) {
+    if (event.code == "Numpad7" || event.code == "Digit7") {
+
 
         mostrarRespuesta('opt7')
     }
-    if (event.keyCode == 56) {
+    if (event.code == "Numpad8" || event.code == "Digit8") {
 
         mostrarRespuesta('opt8')
-        
+
     }
-    if (event.keyCode == 57) {
+    if (event.code == "ArrowRight") {
 
         cargarPregunta(1)
-        
+
     }
-    if (event.keyCode == 48) {
+    if (event.code == "ArrowLeft") {
 
         cargarPregunta(-1)
     }
@@ -126,10 +126,15 @@ function mostrarRespuesta(opt) {
 
 
     } else {
-        if (contador.innerText == "100") {
+        if (contador.innerText == conseguirSuma(preguntas[preguntaActual])) {
             animacionContenedor("grandeMal")
             bigErrorAudio.currentTime = 0
             bigErrorAudio.play();
+        } else {
+            opcionActual.innerHTML = '<div class="optionNumber">' + opt.substr(3, 3) + '</div>'
+            opcionActual.classList.remove("correcto")
+            let nuevaSumaAContador = parseInt(contador.innerText) - preguntas[preguntaActual][opt][1]
+            contador.innerText = nuevaSumaAContador
         }
     }
 }
